9dc3a26e3758e9ced0e8ce0b147ca1165afa62ca
danhlt2 <onljne48h@gmail.com>
2017-02-17 20:37:30 +0700

    FF

--- js/controller/AdminController.js
+++ js/controller/AdminController.js
@@ -1,6 +1,25 @@
-/**
- * Created by Van on 02/02/2017.
- */
-SWDApp.controller('AdminController', function($scope,$controller) {
+SWDApp.controller('AdminController', function($scope,$mdDialog,$rootScope,MovieService,$controller) {
+
     $controller('BaseController', {$scope: $scope});
+
+    initController();
+    function initController() {
+        initView();
+        initData();
+    }
+
+    function initView() {
+        $rootScope.view = SCREEN_MOVIE;
+        if (!$rootScope.selectIndex) {
+            $rootScope.selectIndex = 0;
+        }
+    }
+
+    function initData() {
+        $scope.listFilm = JSON.parse(localStorage.getItem('MOVIE_LIST'));
+    }
+
+    $scope.setData = function(value){
+        MovieService.setItem(value);
+    }
 });
