f266daaf400b3e78135f9981ead14deccaeb850b
danhlt2 <onljne48h@gmail.com>
2017-02-20 09:07:12 +0700

    Navbar,Sort,ButtonAdd

--- js/controller/AdminController.js
+++ js/controller/AdminController.js
@@ -22,4 +22,41 @@ SWDApp.controller('AdminController', function($scope,$mdDialog,$rootScope,MovieS
     $scope.setData = function(value){
         MovieService.setItem(value);
     }
+
+
+    // SortData
+
+    $scope.sortColumn = 'name';
+    $scope.reverse = false;
+
+    $scope.sortData = function(column){
+        if($scope.sortColumn == column)
+            $scope.reverse = !$scope.reverse;
+        else
+            $scope.reverse = false;
+        $scope.sortColumn = column;
+    }
+    $scope.getSortClass = function (column) {
+        if($scope.sortColumn == column){
+            return $scope.reverse ? 'arrow-up':'arrow-down'
+        }
+        return '';
+    }
+
+    // Button AddFilm
+
+
+    var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Tab
+
+
+
 });
