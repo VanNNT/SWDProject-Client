38c86e94927ec71e27a77b9d01b6b19496f5379f
vannntse61626\VanNNT <vannnt2112@gmail.com>
2017-02-22 11:20:41 +0700

    add IndexController

--- js/controller/HomeController.js
+++ js/controller/HomeController.js
@@ -17,7 +17,6 @@ SWDApp.controller('HomeController', function($scope, $mdDialog, $mdMedia,$locati
         $rootScope.currentPage = HOME_PAGE;
         $rootScope.view = 0;
         $rootScope.selectIndex=0;
-        $scope.lang = LANG_EN;
     }
 
     function getSoonSuccess(response){
@@ -31,26 +30,6 @@ SWDApp.controller('HomeController', function($scope, $mdDialog, $mdMedia,$locati
         BaseService.getAPI(URL_MOVIE,'',getSoonSuccess, getSoonFail);
     }
 
-    $scope.showLoginFrom = function (ev) {
-        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-        $mdDialog.show({
-            controller: 'LoginController',
-            templateUrl: 'view/login.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true,
-            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-        }).then();
-    };
-
-    $scope.changeLanguage = function (key) {
-        $translate.use(key);
-        if(key=='en'){
-            $scope.lang = LANG_EN;
-        }else{
-            $scope.lang = LANG_VI;
-        }
-    };
 
     $scope.go = function (value) {
         if(value ==1){
@@ -64,7 +43,6 @@ SWDApp.controller('HomeController', function($scope, $mdDialog, $mdMedia,$locati
     $scope.setData = function(value){
         MovieService.setItem(value);
     };
-    
     $scope.logoutConfirm = function(){
         $scope.showConfirm('','Logout', "Are you sure to logout?",function () {
             $rootScope.logged = false;
