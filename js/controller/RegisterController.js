e3b8f90ddbf385048cea70e2bca5a4705ba91103
Duytnbse <duytnbse61589@fpt.edu.vn>
2017-03-15 10:16:08 +0700

    Nhi test dumf nha

--- js/controller/RegisterController.js
+++ js/controller/RegisterController.js
@@ -1,7 +1,7 @@
 /**
  * Created by VanNNT on 3/3/2017.
  */
-SWDApp.controller('RegisterController', function ($scope, $controller) {
+SWDApp.controller('RegisterController', function ($scope, $controller, $translate, BaseService) {
     $controller('BaseController', {$scope: $scope});
 
     initController();
@@ -11,8 +11,34 @@ SWDApp.controller('RegisterController', function ($scope, $controller) {
             inputBirthday: new Date()
         };
     }
+    function SignUpSuccess(response) {
+        if(!response.data.errorCode){
+           console.log("response")
+            console.log(response.data);
+            $scope.showAlert('', $translate.instant('Successful'), $translate.instant('Your account register successfully'));
+            setTimeout(function(){
+                 
+                window.location.href= "/index.html";
+            }, 500);
+           
+            
+        }
+        else {
+            $scope.showAlert('', $translate.instant('message.error'), $translate.instant(response.data.message)); 
+        }
+            
+    }
+    
+    function SignUpFail(){
+         $scope.showAlert('', $translate.instant('message.error'), $translate.instant('Error'));
+    }
+    
     $scope.signup = function () {
+       
         console.log($scope.userRegister.inputBirthday.toISOString().substr(0, 10));
         console.log($scope.userRegister);
+        $scope.userRegister.birthdate = $scope.userRegister.inputBirthday.toISOString().substr(0, 10);
+        BaseService.postAPI(URL_REGISTER, $scope.userRegister, SignUpSuccess, SignUpFail);
+        
     };
 });
